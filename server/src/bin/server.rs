06d7194ab5343dd00709ad362facca60425a433c
Matt Harrington <querry43@gmail.com>
2018-04-17 03:48:47 +0000

    rebranding assigato server software

---
+++ server/src/bin/server.rs
@@ -0,0 +1,68 @@
+extern crate paper_dog_server;
+extern crate pi_robot;
+extern crate ws;
+
+use std::sync::{Arc, Mutex};
+use std::thread::sleep;
+use std::thread;
+use std::time::Duration;
+use ws::{listen, Handler, Sender, Handshake, CloseCode};
+
+struct Server {
+    out: Sender,
+    robot: Arc<Mutex<pi_robot::Robot>>,
+}
+
+impl Handler for Server {
+    fn on_open(&mut self, _: Handshake) -> ws::Result<()> {
+        println!("on_open: {:?}", self.out.token());
+        let r = self.robot.lock().unwrap();
+        self.out.send(r.state.to_string())
+    }
+
+    fn on_message(&mut self, msg: ws::Message) -> ws::Result<()> {
+        let m = paper_dog_server::Message::from(msg.as_text()?);
+		println!("on_message: {:?}", m);
+        let mut r = self.robot.lock().unwrap();
+
+        let res = match m {
+            paper_dog_server::Message::PWMChannelState(msg) => r.update_pwm_channel(msg),
+            paper_dog_server::Message::ShiftRegisterState(msg) => r.update_shift_register(msg),
+            paper_dog_server::Message::RobotSpeak(msg) => r.robot_speak(msg),
+        };
+
+        match res {
+            Ok(()) => (),
+            Err(err) => println!("Encountered error: {:?}", err),
+        }
+
+        self.out.broadcast(r.state.to_string())
+    }
+
+    fn on_close(&mut self, code: CloseCode, reason: &str) {
+        match code {
+            _ => println!("The client disconnected: {:?} {}", self.out.token(), reason),
+        }
+    }
+}
+
+
+fn main() {
+    let robot = Arc::new(Mutex::new(pi_robot::Robot::new("config/robot.yaml").unwrap()));
+    println!("{:?}", robot);
+	spawn_robot_update_thread(robot.clone());
+	listen("0.0.0.0:3000", |out| Server { out: out, robot: robot.clone() } ).unwrap()
+} 
+
+fn spawn_robot_update_thread(robot: Arc<Mutex<pi_robot::Robot>>) {
+    thread::spawn(move || {
+        loop {
+            sleep(Duration::from_millis(50));
+            let mut r = robot.lock().unwrap();
+            match r.refresh() {
+                Err(err) => println!("refresh error: {:?}", err),
+                Ok(_) => (),
+            }
+        }
+    });
+}
---
+++ server/src/lib.rs
@@ -0,0 +1,27 @@
+extern crate pi_robot;
+extern crate serde_json;
+
+use std::fmt;
+
+#[macro_use]
+extern crate serde_derive;
+
+#[derive(Serialize, Deserialize, Debug)]
+pub enum Message {
+    PWMChannelState(pi_robot::PWMChannelState),
+    ShiftRegisterState(pi_robot::ShiftRegisterState),
+    RobotSpeak(pi_robot::RobotSpeak),
+}
+
+impl fmt::Display for Message {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{}", serde_json::to_string(self).unwrap())
+    }
+}
+
+impl<'a> From<&'a str> for Message {
+    fn from(s: &str) -> Self {
+        serde_json::from_str(&s).unwrap()
+    }
+}
+
